53d0a93269446e8e4a8e051b8ee1de5e9e0cea76
Karina Gaulin <karinagaulin@gmail.com>
2017-04-25 13:38:53 -0400

    Redone

--- app.js
+++ app.js
@@ -2,26 +2,23 @@ $(document).ready(function() {
 	var move = 1;
 	var play = true;
 
-	$("td").click(function() {
-		console.log(this);
-		if($(this).text()=="" && play) {
-			if((move%2)==1) {
-				$(this).append('X');
-			} else {
-				$(this).append('O');
-			}
-			move++;
-			if(checkForWinner() !=-1 && checkForWinner() !="") {
-				if(checkForWinner() == 'X') {
-					alert('Player 1 wins!');
-				} else {
-					alert('Player 2 wins!');
-				}
-				play = false;
-			}
+	$('td').click(function() {
+		if(move%2==1) {
+			$(this).html("O");
+		} else {
+			$(this).html("X");
 		}
-	})
-	function checkForWinner () {
+		move++
+		var winner = checkForWinner();
+		if(winner !== -1 && winner !== undefined) {
+			alert(winner + " is the winner!");
+		} else if(winner !== undefined) { 
+
+			alert("Draw!");
+		}
+	});
+
+	function checkForWinner() {
 		var space1 = $('#r1c1').text();
     	var space2 = $('#r1c2').text();
     	var space3 = $('#r1c3').text();
@@ -34,27 +31,44 @@ $(document).ready(function() {
     	console.log(space7);
 	//check rows
 		if((space1 == space2) && (space2==space3)) {
-			return space3;
+			if(space3 == "X" || space3 == "O") {
+				return space3;
+			}
 		} else if((space4==space5) && (space5 == space6)) {
-			return space6;
+			if(space6 == "X" || space6 == "O") {
+				return space6;
+			}
 		} else if((space3==space8) && (space8==space9)) {
-			return space9;
+			if(space9 == "X" || space9 == "O") {
+				return space9;
+			}
 		}
 	//check columns
 		else if ((space1==space4) && (space4 ==space7)) {
-			return space7;
+			if(space7 == "X" || space7 == "O") {
+				return space7;
+			}
 		} else if((space2==space5) && (space5 ==space8)) {
-			return space8;
+			if(space8 == "X" || space8 == "O") {
+				return space8;
+			}
 		} else if((space3==space6) && (space6==space9)) {
-			return space9;
+			if(space9 == "X" || space9 == "O") {
+				return space9;
+			}
 		}
 	//check diagonals
 		else if((space1==space5) && (space5==space9)) {
-			return space9;
+			if(space9 == "X" || space9 == "O") {
+				return space9;
+			}
 		} else if((space3==space5) && (space5==space7)) {
-			return space7;
+			if(space7 == "X" || space7 == "O") {
+				return space7;
+			}
 		}
+	}
 	//no winner
 		return -1
-	}
+
 });
